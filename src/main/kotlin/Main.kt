425934c4d2e83f5c287fdd50e202a02f02ddf714
Wato-Adano <watobuchaadano@gmail.com>
2024-03-25 08:21:30 +0300

    add file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,44 @@
+fun main() {
+ word("Barnie Bakes Brown Bagels and Burn")
+    var sum=(20, 30, 40)
+    sentence2("Palindrome")
+
+}
+
+fun word(sentence:String){
+    var sentences=("Barnie bakes brown bagels and burn")
+    println(sentence)
+
+}
+
+fun numbers(word1:Int, word2:Int,word3:Int){
+    var sum=arrayOf(word1, word2, word3)
+    return(sum)
+
+}
+fun volume(num1:Int){
+   var radius=4/3*3.14159
+}
+
+fun sentence2(words:String):Boolean{
+    var words=(if words=="Palindrome")
+    println(true)
+    else println (false)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
